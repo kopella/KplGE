f9b1f35379932957ba32b40ddc6529e06f45fdc0
Kopella <kopella.pvt@gmail.com>
2023-02-15 14:25:12 +0800

    Daily commit

---
+++ framework/common/asset-loader.h
@@ -0,0 +1,10 @@
+#include "runtime-module.h"
+#include "buffer.h"
+
+namespace kplge {
+class AssetLoader : IRuntimeModule {
+ public:
+  AssetLoader();
+};
+
+}  // namespace kplge
---
+++ framework/common/buffer.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <cstddef>
+#include <cstdlib>
+#include <cstring>
+#include <memory>
+
+namespace kplge {
+class Buffer {
+ public:
+  Buffer() = default;
+};
+}  // namespace kplge
